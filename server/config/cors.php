64320804da157e196f65bb52f12912406627dff1
MD Jaliz Mahamud Mridul <139718645+moonstruck2003@users.noreply.github.com>
2026-04-12 00:55:31 +0600

    Fix: Allow all Vercel domains in CORS to support Preview deployments

--- server/config/cors.php
+++ server/config/cors.php
@@ -26,7 +26,9 @@ return [
         'http://127.0.0.1:5174',
     ],
 
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        '#^https://.*\.vercel\.app$#'
+    ],
 
     'allowed_headers' => ['*'],
 
